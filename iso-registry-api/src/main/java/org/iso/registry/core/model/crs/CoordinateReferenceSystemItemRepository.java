e4d63f6a63a6575235d3ff6770c03b90a09afa4a
Florian Esser <florian.esser@geoinfoffm.de>
2014-05-18 20:52:45 +0200

    Moved HibernateConfiguration bean to client project
    Former-commit-id: 70a4c373328f38d611b1a133f4f35aa87623cc02

--- iso-registry-api/src/main/java/org/iso/registry/core/model/crs/CoordinateReferenceSystemItemRepository.java
+++ iso-registry-api/src/main/java/org/iso/registry/core/model/crs/CoordinateReferenceSystemItemRepository.java
@@ -1,9 +1,10 @@
 package org.iso.registry.core.model.crs;
 
-import org.iso.registry.core.model.cs.CoordinateSystemItem;
+import org.springframework.stereotype.Repository;
 
 import de.geoinfoffm.registry.core.EntityRepository;
 
+@Repository
 public interface CoordinateReferenceSystemItemRepository extends EntityRepository<CoordinateReferenceSystemItem>
 {
 	CoordinateReferenceSystemItem findByCode(Integer code);
