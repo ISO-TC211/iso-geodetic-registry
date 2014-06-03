7e0431b0b3f4169f74aba42be8c63a0274f3717f
Florian Esser <florian.esser@geoinfoffm.de>
2014-06-03 21:23:47 +0200

    Extracted ParameterValueDTO to own class file
    Former-commit-id: 04c587fdf14735db5c4363401263c80c85a834fd

--- iso-registry-api/src/main/java/org/iso/registry/api/registry/registers/gcp/operation/SingleOperationItemProposalDTO.java
+++ iso-registry-api/src/main/java/org/iso/registry/api/registry/registers/gcp/operation/SingleOperationItemProposalDTO.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.UUID;
 
 import javax.persistence.EntityManager;
 
@@ -15,7 +14,6 @@ import org.iso.registry.core.model.operation.OperationMethodItem;
 import org.iso.registry.core.model.operation.OperationParameterItem;
 import org.iso.registry.core.model.operation.OperationParameterValue;
 import org.iso.registry.core.model.operation.SingleOperationItem;
-import org.iso.registry.core.model.operation.OperationParameterValue.OperationParameterValueType;
 import org.isotc211.iso19135.RE_RegisterItem_Type;
 import org.springframework.util.StringUtils;
 
@@ -31,93 +29,8 @@ public class SingleOperationItemProposalDTO extends CoordinateOperationItemPropo
 		TRANSFORMATION
 	}
 	
-	public static class ParameterValueDTO {
-		
-		private UUID parameterUuid;
-		private String parameterName;
-		private OperationParameterValueType parameterType;
-		private String value;
-		private UUID uomUuid;
-		
-		public ParameterValueDTO() { }
-
-		public ParameterValueDTO(UUID parameterUuid, String parameterName, OperationParameterValueType parameterType, String value) {
-			this.parameterUuid = parameterUuid;
-			this.setParameterName(parameterName);
-			this.value = value;
-			this.parameterType = parameterType;
-			this.uomUuid = null;
-		}
-
-		public ParameterValueDTO(UUID parameterUuid, String parameterName, String value, UUID uomUuid) {
-			this.parameterUuid = parameterUuid;
-			this.setParameterName(parameterName);
-			this.value = value;
-			this.uomUuid = uomUuid;
-			if (this.uomUuid != null) {
-				this.parameterType = OperationParameterValueType.MEASURE;
-			}
-			else {
-				this.parameterType = OperationParameterValueType.STRING;
-			}
-		}
-
-		public UUID getParameterUuid() {
-			return parameterUuid;
-		}
-
-		public void setParameterUuid(UUID parameterUuid) {
-			this.parameterUuid = parameterUuid;
-		}
-
-		/**
-		 * @return the parameterName
-		 */
-		public String getParameterName() {
-			return parameterName;
-		}
-
-		/**
-		 * @param parameterName the parameterName to set
-		 */
-		public void setParameterName(String parameterName) {
-			this.parameterName = parameterName;
-		}
-
-		/**
-		 * @return the parameterType
-		 */
-		public OperationParameterValueType getParameterType() {
-			return parameterType;
-		}
-
-		/**
-		 * @param parameterType the parameterType to set
-		 */
-		public void setParameterType(OperationParameterValueType parameterType) {
-			this.parameterType = parameterType;
-		}
-
-		public String getValue() {
-			return value;
-		}
-
-		public void setValue(String value) {
-			this.value = value;
-		}
-
-		public UUID getUomUuid() {
-			return uomUuid;
-		}
-
-		public void setUomUuid(UUID uomUuid) {
-			this.uomUuid = uomUuid;
-		}
-	}
-	
 	private SingleOperationType operationType;
 	private OperationMethodItemProposalDTO method;
-//	private List<GeneralParameterValue<?>> parameterValue;
 	private List<ParameterValueDTO> parameterValues;
 
 	public SingleOperationItemProposalDTO() {
@@ -168,13 +81,13 @@ public class SingleOperationItemProposalDTO extends CoordinateOperationItemPropo
 					}
 				}
 				for (ParameterValueDTO parameterValue : this.getParameterValues()) {
-					OperationParameterItem parameter = entityManager.find(OperationParameterItem.class, parameterValue.parameterUuid);
+					OperationParameterItem parameter = entityManager.find(OperationParameterItem.class, parameterValue.getParameterUuid());
 					
-					String paramValue = parameterValue.value;
+					String paramValue = parameterValue.getValue();
 					
 					UnitOfMeasureItem uom = null;
-					if (parameterValue.uomUuid != null) {
-						uom = entityManager.find(UnitOfMeasureItem.class, parameterValue.uomUuid);
+					if (parameterValue.getUomUuid() != null) {
+						uom = entityManager.find(UnitOfMeasureItem.class, parameterValue.getUomUuid());
 					}
 					
 					OperationParameterValue value;
