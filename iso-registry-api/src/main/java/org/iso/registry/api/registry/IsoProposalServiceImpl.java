65e7808a98150f40faaeba37e0c89e6914882ce3
Florian Esser <fe@bespire.de>
2014-10-26 12:41:34 +0100

    Added deleteProposal to IsoProposalService
    Former-commit-id: eeb0c7aa043015ffed540b9f1f7d77a9ddc3c9f0

--- iso-registry-api/src/main/java/org/iso/registry/api/registry/IsoProposalServiceImpl.java
+++ iso-registry-api/src/main/java/org/iso/registry/api/registry/IsoProposalServiceImpl.java
@@ -1,5 +1,9 @@
 package org.iso.registry.api.registry;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.iso.registry.core.model.ProposalNote;
 import org.iso.registry.core.model.ProposalNoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +15,14 @@ import de.geoinfoffm.registry.core.UnauthorizedException;
 import de.geoinfoffm.registry.core.model.Proposal;
 import de.geoinfoffm.registry.core.model.RegistryUser;
 import de.geoinfoffm.registry.core.model.iso19135.InvalidProposalException;
+import de.geoinfoffm.registry.core.model.iso19135.ProposalManagementInformationRepository;
+import de.geoinfoffm.registry.core.model.iso19135.RE_ItemStatus;
 import de.geoinfoffm.registry.core.model.iso19135.RE_ProposalManagementInformation;
+import de.geoinfoffm.registry.core.model.iso19135.RE_RegisterItem;
 import de.geoinfoffm.registry.core.security.RegistrySecurity;
 import de.geoinfoffm.registry.persistence.ProposalRepository;
+import de.geoinfoffm.registry.persistence.RegisterItemRepository;
 
-//@Service
 public class IsoProposalServiceImpl extends ProposalServiceImpl implements IsoProposalService
 {
 	public static final String STATUS_RETURNED_BY_MANAGER = "RETURNED_BY_MANAGER";
@@ -25,11 +32,53 @@ public class IsoProposalServiceImpl extends ProposalServiceImpl implements IsoPr
 	private ProposalNoteRepository noteRepository;
 	
 	@Autowired
+	private ProposalManagementInformationRepository pmiRepository;
+	
+	@Autowired
+	private RegisterItemRepository itemRepository;
+	
+	@Autowired
 	private RegistrySecurity security;
 	
 	@Autowired
 	public IsoProposalServiceImpl(ProposalRepository repository) {
 		super(repository);
+	}
+
+	@Override
+	@Transactional
+	public void deleteProposal(Proposal proposal) throws IllegalOperationException, UnauthorizedException {
+		if (proposal == null) {
+			throw new IllegalArgumentException("Cannot return null proposal.");
+		}
+
+		if (proposal.isSubmitted()) {
+			throw new IllegalOperationException("Cannot delete submitted proposals: use withdraw");			
+		}
+		
+		List<RE_RegisterItem> itemsToDelete = new ArrayList<>();
+		List<RE_ProposalManagementInformation> pmisToDelete = new ArrayList<>();
+		
+		for (RE_ProposalManagementInformation pmi : proposal.getProposalManagementInformations()) {
+			if (pmi.getItem() != null && RE_ItemStatus.NOT_VALID.equals(pmi.getItem().getStatus())) {
+				RE_RegisterItem item = pmi.getItem();
+				pmi.setItem(null);
+				itemsToDelete.add(item);
+			}
+
+			pmisToDelete.add(pmi);
+		}
+		
+		proposal.delete();
+		repository().delete(proposal);
+		
+		for (RE_ProposalManagementInformation pmi : pmisToDelete) {
+			pmiRepository.delete(pmi);
+		}
+
+		for (RE_RegisterItem item : itemsToDelete) {
+			itemRepository.delete(item);
+		}
 	}
 
 	@Override
@@ -60,7 +109,10 @@ public class IsoProposalServiceImpl extends ProposalServiceImpl implements IsoPr
 		for (RE_ProposalManagementInformation pmi : proposal.getProposalManagementInformations()) {
 			pmi.setDateProposed(null);
 		}
-		this.createNote(proposal, author, noteToSubmitter);
+		
+		if (!StringUtils.isEmpty(noteToSubmitter)) {
+			this.createNote(proposal, author, noteToSubmitter);
+		}
 
 		proposal = repository().save(proposal);
 		return proposal;
@@ -72,6 +124,5 @@ public class IsoProposalServiceImpl extends ProposalServiceImpl implements IsoPr
 		
 		return noteRepository.save(result);
 	}
-	
 	
 }
