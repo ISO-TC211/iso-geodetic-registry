d27cd2bd60e6d6bf45910627e9ba2f18ddf70d10
Florian Esser <fe@bespire.de>
2017-05-25 15:28:53 +0200

    Add handling of parameter values
    Former-commit-id: 7b1d3bf536d4a1c4fa650fa593eb0c81d9c8c9a5

--- src/iso-registry-api/src/main/java/org/iso/registry/api/registry/registers/gcp/operation/SingleOperationItemProposalDTO.java
+++ src/iso-registry-api/src/main/java/org/iso/registry/api/registry/registers/gcp/operation/SingleOperationItemProposalDTO.java
@@ -187,9 +187,9 @@ public class SingleOperationItemProposalDTO extends CoordinateOperationItemPropo
 				this.setMethod(new OperationMethodItemProposalDTO(item.getMethod()));
 				if (item.getParameterValue() != null) {
 					this.parameterValues = new ArrayList<>();
-//					for (GeneralParameterValue<?> parameterValue : item.getParameterValue()) {
-//						this.addParameterValue(parameterValue);
-//					}
+					for (GeneralParameterValue<?> parameterValue : item.getParameterValue()) {
+						this.addParameterValue(parameterValue);
+					}
 				}
 			}
 		}
