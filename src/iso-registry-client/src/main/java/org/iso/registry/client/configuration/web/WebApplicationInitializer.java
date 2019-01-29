234de90ad1cf22cbf8acd23bc51d103de5d4098c
phuong <phuonghqh@gmail.com>
2019-01-29 18:57:23 +0000

    Dockernize the app
    Former-commit-id: 0e423ed4fd1a505e02069c57bee221cc7d7cb900 [formerly 6d81345b6514675e594a25e78864c10f36c8d445]
    Former-commit-id: c2ad6316dbac396a383c8f8cff1d882f424ed4ee

--- src/iso-registry-client/src/main/java/org/iso/registry/client/configuration/web/WebApplicationInitializer.java
+++ src/iso-registry-client/src/main/java/org/iso/registry/client/configuration/web/WebApplicationInitializer.java
@@ -3,11 +3,14 @@ package org.iso.registry.client.configuration.web;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import de.geoinfoffm.registry.persistence.jpa.HibernateConfigurationImpl;
 import org.iso.registry.client.configuration.security.SecurityConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 import de.geoinfoffm.registry.client.web.AbstractWebApplicationInitializer;
 import de.geoinfoffm.registry.persistence.PersistenceConfiguration;
+
+import java.sql.*;
 
 /**
  * Initializer for the Spring web application.
@@ -37,6 +40,46 @@ public class WebApplicationInitializer extends AbstractWebApplicationInitializer
 	@Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         super.onStartup(servletContext);
-        servletContext.addListener(new SessionListener());
+
+		HibernateConfigurationImpl conf = new HibernateConfigurationImpl();
+
+		try {
+			Connection connection  = conf.dataSource().getConnection();
+			int count = 0;
+			while (!this.isDbConnected(connection) && count < 10) {
+				Thread.sleep(10_000);
+				System.out.println("Waiting count= " + count++);
+			}
+
+			if (count >= 10) {
+				throw new ServletException("DB failed (count expired)");
+			}
+
+			System.out.println("DB is ready!!");
+		} catch (Exception e) {
+			System.out.println("Connection Failed! Check output console");
+			throw new ServletException("DB failed", e);
+
+		}
+
+
+		servletContext.addListener(new SessionListener());
     }
+
+	public boolean isDbConnected(Connection connection) {
+		final String CHECK_SQL_QUERY = "SELECT 1;";
+		boolean isConnected = false;
+		try {
+			final PreparedStatement statement = connection.prepareStatement(CHECK_SQL_QUERY);
+//			ResultSet resultSet = statement.executeQuery();
+//			resultSet.next();
+//			System.out.println("Count registryuser= " + resultSet.getInt(1));
+			isConnected = true;
+		} catch (Exception e) {
+			// handle SQL error here!
+			System.err.println(e);
+			System.out.println("DB is not ready => waiting");
+		}
+		return isConnected;
+	}
 }
