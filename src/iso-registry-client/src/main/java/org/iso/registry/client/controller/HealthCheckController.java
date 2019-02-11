d4a9b91f85d65a65743d496203c18b007e9b8409
Phuong H <phuonghqh@gmail.com>
2019-02-11 14:30:56 +0700

    Add simple Healthy check
    Former-commit-id: 324aab07273f758a7e78f27470f874f400592416 [formerly 57a800fc6d4422023bec9c9a32c5081d5513cf6c]
    Former-commit-id: 1e2bda33ea451f5bdbb475ebf88e004df97be283

---
+++ src/iso-registry-client/src/main/java/org/iso/registry/client/controller/HealthCheckController.java
@@ -0,0 +1,18 @@
+package org.iso.registry.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@Controller
+public class HealthCheckController {
+
+    @ResponseStatus(OK)
+    @RequestMapping(value = "/ping", method = RequestMethod.HEAD)
+    public void ping() {
+        System.out.println("Pinging me. I m OK.");
+    }
+}
