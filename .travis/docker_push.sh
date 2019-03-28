9ff29de1d871dfcc38fe32bd25b1857efe91077d
phuong <phuonghqh@gmail.com>
2019-03-28 20:44:14 +0700

    Update travis

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "registry= ${REGISTRY}"
+echo "DOCKER_TAG= ${DOCKER_TAG}"
 
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin registry.gitlab.com
 
-docker push registry.gitlab.com/tc211/geodetic-registry-docker/web:${TRAVIS_BUILD_NUMBER}
+docker push ${DOCKER_TAG}
