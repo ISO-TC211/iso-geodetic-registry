f8687a9169e9c3ee27b800cd92470939c1e851bf
phuong <phuonghqh@gmail.com>
2019-05-29 23:19:39 +0700

    Update logo ribose

--- src/iso-registry-client/src/main/java/org/iso/registry/client/configuration/security/SecurityConfiguration.java
+++ src/iso-registry-client/src/main/java/org/iso/registry/client/configuration/security/SecurityConfiguration.java
@@ -55,7 +55,7 @@ import de.geoinfoffm.registry.persistence.jpa.HibernateConfiguration;
  */
 @Configuration
 @ComponentScan(basePackages = { "org.iso.registry", "de.bespire.registry", "de.geoinfoffm.registry" })
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 {
