e4bad8c23941111a51f283c5cb7b8aaf7ce67069
phuong <phuonghqh@gmail.com>
2020-01-15 16:26:04 +0700

    Add react template

---
+++ src/iso-registry-client-boot/src/main/java/org/iso/registry/client/boot/Main.java
@@ -0,0 +1,13 @@
+package org.iso.registry.client.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
