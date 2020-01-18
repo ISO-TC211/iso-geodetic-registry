72f12cd0376187d0a6b7075208768b4315dc1e65
phuong <phuonghqh@gmail.com>
2020-01-18 16:06:24 +0700

    Add assets files

--- src/iso-registry-frontend/src/app/app.module.ts
+++ src/iso-registry-frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
