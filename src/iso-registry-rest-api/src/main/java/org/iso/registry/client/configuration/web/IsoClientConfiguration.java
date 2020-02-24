fa9cf3153435b9905d292870f2e30accb6dba85b
phuong <phuonghqh@gmail.com>
2020-02-24 23:57:08 +0700

    Remove some unused dependencies

--- src/iso-registry-rest-api/src/main/java/org/iso/registry/client/configuration/web/IsoClientConfiguration.java
+++ src/iso-registry-rest-api/src/main/java/org/iso/registry/client/configuration/web/IsoClientConfiguration.java
@@ -1,52 +1,24 @@
 package org.iso.registry.client.configuration.web;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-
-import de.geoinfoffm.registry.client.web.ClientConfiguration;
-import de.geoinfoffm.registry.client.web.RegistryClientViewResolver;
-import de.geoinfoffm.registry.core.model.iso19103.CharacterString;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.repository.support.DomainClassConverter;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.format.support.FormattingConversionService;
 import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
-import org.springframework.orm.hibernate4.support.OpenSessionInViewInterceptor;
-import org.springframework.orm.jpa.support.OpenEntityManagerInViewInterceptor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.util.StringUtils;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.*;
-
-import de.geoinfoffm.registry.client.web.AbstractWebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-import javax.persistence.EntityManagerFactory;
+import java.util.List;
 
 /**
  * Spring configuration class for the GDI-DE Registry Client.
