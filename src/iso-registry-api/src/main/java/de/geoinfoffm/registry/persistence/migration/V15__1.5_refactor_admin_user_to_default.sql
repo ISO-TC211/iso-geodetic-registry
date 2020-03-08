aa779a10f7682d9e561a30f0d962363506abd7c6
Florian Esser <fe@florianesser.eu>
2020-03-08 13:56:13 +0100

    fix: don't manipulate admin user as part of the database migration
    Changing the admin user must not be done as part of the database
    migration mechanism. This change is specific to the Ribose environment
    and does not apply to other environments.

--- src/iso-registry-api/src/main/java/de/geoinfoffm/registry/persistence/migration/V15__1.5_refactor_admin_user_to_default.sql
+++
@@ -1,7 +0,0 @@
--- admin@example.org change to "geodetic.register@ribose.com" , reset password "a"
--- this is to reset as Ribose required on May 24 2019
-
-UPDATE "public"."registryuser"
-SET "emailaddress" = 'geodetic.register@ribose.com',
-    "passwordhash" = '$2a$12$w8AoYGP2UxTsnrajt0UJ6eExjm1AD/IG2RD87wx5qoXkKXiCEgYT6'
-WHERE "emailaddress" = 'admin@example.org';
