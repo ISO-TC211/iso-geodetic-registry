9c88772badcd6019ae34290a2bd183e6e72cd29e
brian behling <brian.behling@gmail.com>
2020-03-16 12:48:07 -0600

    Tree shaking ng bootstrap

--- src/iso-registry-frontend/src/app/app.module.ts
+++ src/iso-registry-frontend/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from "./home/home.component";
 import { HeaderComponent } from "./header/header.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-
+import {
+  NgbDropdownModule,
+  NgbCollapseModule
+} from "@ng-bootstrap/ng-bootstrap";
 @NgModule({
   declarations: [AppComponent, FooterComponent, HomeComponent, HeaderComponent],
   imports: [
-    NgbModule,
+    NgbDropdownModule,
+    NgbCollapseModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
