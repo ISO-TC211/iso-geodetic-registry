ecbea90e106716caf20c215f43881e5e823a87b7
phuong <phuonghuynh@users.noreply.github.com>
2020-04-18 12:25:54 +0700

    Prepare to release new docker image to Github Package (#78)
    * Add log test code
    
    * Return 204 in healthy check
    
    * Add log-level to dockerfile
    
    * Use catalina_base env
    
    * Fix docker build
    
    * Add latest tag release to GHA

--- Dockerfile
+++ Dockerfile
@@ -10,3 +10,9 @@ FROM tomcat:8-jre8-alpine
 RUN value=`cat /usr/local/tomcat/conf/server.xml` && echo "${value//8080/80}" >| /usr/local/tomcat/conf/server.xml
 RUN rm -Rf /usr/local/tomcat/webapps
 COPY --from=APP /app/dist/isoreg.war /usr/local/tomcat/webapps/ROOT.war
+RUN echo -e "\norg.apache.level=WARNING" >> /usr/local/tomcat/conf/logging.properties
+RUN echo -e "\norg.apache.catalina.startup.Catalina.level=INFO" >> /usr/local/tomcat/conf/logging.properties
+RUN echo -e "\norg.hibernate.level=WARNING" >> /usr/local/tomcat/conf/logging.properties
+#org.apache.level=WARNING
+#org.apache.catalina.startup.Catalina.level=INFO
+#org.hibernate.level=WARNING
--- src/iso-registry-client/src/main/java/org/iso/registry/client/controller/HealthCheckController.java
+++ src/iso-registry-client/src/main/java/org/iso/registry/client/controller/HealthCheckController.java
@@ -1,18 +1,24 @@
 package org.iso.registry.client.controller;
 
+import de.bespire.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.http.HttpServletResponse;
+
 import static org.springframework.http.HttpStatus.OK;
 
 @Controller
 public class HealthCheckController {
+    private static final Logger logger = LoggerFactory.make();
 
     @ResponseStatus(OK)
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
-    public void ping() {
-        System.out.println("Pinging me. I m OK.");
+    public void ping(HttpServletResponse response) {
+        logger.debug("Pinging me. I m OK.");
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
     }
 }
